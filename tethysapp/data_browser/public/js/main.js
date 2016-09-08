17b777b3f662bbb302aedf62ba5df3c497ceeaf3
Scott D. Christensen <scott.d.christensen@erdc.dren.mil>
2016-09-08 15:00:16 -0500

    delete dataset

--- tethysapp/data_browser/public/js/main.js
+++ tethysapp/data_browser/public/js/main.js
@@ -1,30 +1,65 @@
 
-function update_details_table(collection_name, html){
-    $('#collection-detail-' + collection_name).replaceWith(html);
-    // hack to get new details table to show
-    // TODO doesn't work when there is only one collection
-    $('#collection-details-nav a:first').tab('show');
-    $('#collection-details-nav a:last').tab('show');
-    $('li.' + collection_name + '-collection a').tab('show');
-
-
-}
-
-
 /*******************************************************************************
  *
- *                        BUTTON HANDLERS
+ *                        FUNCTIONS
  *
  *******************************************************************************/
 
-// Download/Visualize Options Button
-$('#collection-details-content').on('click', '.get-options', function(e){
+function reload_collection_details_tabs(){
+    // hack to get new details table to show
+    // TODO doesn't work when there is only one collection
+    var active_tab = $('#collection-details-nav li.active');
+    $('#collection-details-nav li a').tab('show')
+    active_tab.tab('show');
+    if(active_tab.length == 0){
+        $('#collection-details-nav li.active').removeClass('active');
+    }
+    if($('#collection-details-nav li a').length == 1){
+        // do something to get it to refresh.
+    }
+}
+
+
+function update_details_table(collection_name, html){
+    $('#collection-detail-' + collection_name).replaceWith(html);
+    reload_collection_details_tabs();
+    bind_context_menu();
+}
+
+function delete_dataset(dataset_id){
+    var url = delete_dataset_url;
+    var csrftoken = getCookie('csrftoken');
+    var data = {dataset: dataset_id,
+                csrfmiddlewaretoken: csrftoken};
+
+    $.post(url, data, function(result){
+        if(result.success){
+            update_details_table(result.collection_name, result.details_table_html);
+        }
+    })
+    .done(function() {
+
+    })
+    .fail(function() {
+        console.log( "error" );
+    })
+    .always(function() {
+
+    });
+}
+
+function get_dataset_id_from_details_table_row(row){
+    var dataset_id = row.children('td').first().text();
+    return dataset_id;
+}
+
+function populate_options_form(event){
     var dataset = $(this).attr('data-dataset-id');
     var data = {'dataset': dataset};
     var type = $(this).attr('data-options-type');
-    var url = {'download': get_download_options_url,
-               'filter': get_filter_options_url,
-               'visualize': visualize_dataset_url,
+    var url = {download: get_download_options_url,
+               filter: get_filter_options_url,
+               visualize: visualize_dataset_url,
                }[type];
 //    $('#options-content').load(url, $.param(data), function(e){
 //        $('.select2').select2();
@@ -48,10 +83,10 @@ $('#collection-details-content').on('click', '.get-options', function(e){
                }, 500);
 
             };
-            var func = {'download': options,
-             'filter': options,
-             'visualize': visualize,
-             }
+            var func = {download: options,
+                        filter: options,
+                        visualize: visualize,
+                        }
 
              func[type]();
 
@@ -66,14 +101,12 @@ $('#collection-details-content').on('click', '.get-options', function(e){
     .always(function() {
 
     });
+}
 
-});
-
-// Download Button
-$('#options-content').on('click', '.options-submit', function(e){
+function submit_options(event){
 
     // submit form to update dataset download options and then download
-    e.preventDefault();
+    event.preventDefault();
     var dataset = $(this).attr('data-dataset-id');
     var url = $('#options-form').attr('action');
     var data = $('#options-form').serializeArray();
@@ -94,8 +127,7 @@ $('#options-content').on('click', '.options-submit', function(e){
     .always(function() {
 
     });
-
-});
+}
 
 function add_collection_details(collection_name, collection_display_name, details_html){
     nav_html = '<li role="presentation" class="nav-tab ' + collection_name + '-collection"><a href="#collection-detail-' + collection_name + '" aria-controls="collection-detail-' + collection_name + '" role="tab" data-toggle="tab">' + collection_name + '</a></li>';
@@ -103,8 +135,8 @@ function add_collection_details(collection_name, collection_display_name, detail
     $('#collection-details-content').append(details_html);
 }
 
-// New Collection Button
-$('#new-collection-form').on('submit', function(event){
+
+function new_collection(event){
     event.preventDefault();
     var url = $(this).attr('action');
     var data = $(this).serializeArray();
@@ -134,10 +166,9 @@ $('#new-collection-form').on('submit', function(event){
     .always(function() {
 
     });
-});
+}
 
-// Delete Collection Link
-$('#collections-list').on('click', '.delete-collection', function(event){
+function delete_collection(event){
     event.preventDefault();
     var url = $(this).attr('href');
     var collection_name = $(this).attr('data-collection-name');
@@ -146,43 +177,58 @@ $('#collections-list').on('click', '.delete-collection', function(event){
     $.get(url, function(result){
         if(result.success){
             $(collection_elements).remove();
+            reload_collection_details_tabs();
             remove_layer(collection_name);
             $('#collection-details-nav a:first').tab('show');
-
+            // update collection select
+            $('#collection option[value="' + collection_name + '"]').remove();
+            $('#collection').select2('val', '');
+            $('#collection').trigger('change');
         }
     });
+}
+
+function bind_context_menu(){
+    $("#collection-details-container td").contextMenu({
+        menuSelector: "#details-context-menu",
+        menuSelected: function (invokedOn, selectedMenu) {
+            var options = {'Download': '',
+                           'Apply Filter': '',
+                           'Visualize': '',
+                           'Delete': delete_dataset,
+                           }
+
+            var option = options[selectedMenu.text()];
+            var dataset_id = get_dataset_id_from_details_table_row(invokedOn.parent());
+            option(dataset_id);
+        }
+    });
+}
+
+
+/*******************************************************************************
+ *
+ *                        BUTTON HANDLERS
+ *
+ *******************************************************************************/
+
+$(function() { //wait for page to load
+
+// Download/Visualize Options Button
+$('#collection-details-content').on('click', '.get-options', populate_options_form);
+
+// Download Button
+$('#options-content').on('click', '.options-submit', submit_options);
+
+// New Collection Button
+$('#new-collection-form').on('submit', new_collection);
+
+// Delete Collection Link
+$('#collections-list').on('click', '.delete-collection', delete_collection);
+
+bind_context_menu();
+
 });
-
-
-
-
-
-//$('#download-dataset-form').submit(function(e){
-//    e.preventDefault();
-//    var url = $(this).attr('action');
-//    var data = $(this).serializeArray();
-//    var selected_features = search_select_interaction.getFeatures();
-//    var features = selected_features.array_.map(function(feature){
-//        return feature.id_;
-//    });
-//    data.push({'name': 'features',
-//               'value': features});
-//
-//    $.get(url, data, function(result){
-//        console.log(result);
-//    })
-//    .done(function() {
-//        $('#add-features-modal').hide();
-//    })
-//    .fail(function() {
-//        console.log( "error" );
-//    })
-//    .always(function() {
-//
-//    });
-//});
-
-
 
 
 /*******************************************************************************
@@ -313,3 +359,100 @@ $('.checkbox-tree input[type="checkbox"]').change(function(e) {
 
   checkSiblings(container);
 });
+
+/*****************************************************************************
+ *
+ * Cross Site Request Forgery Token Configuration
+ *   copied from (https://docs.djangoproject.com/en/1.7/ref/contrib/csrf/)
+ *
+ *****************************************************************************/
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+/*******************************************************************************
+ *
+ *                        CONTEXT MENU
+ *
+ *******************************************************************************/
+
+
+//Context Menu code from: http://jsfiddle.net/kylemit/x9tgy/
+$.fn.contextMenu = function (settings) {
+
+    return this.each(function () {
+
+        // Open context menu
+        $(this).on("contextmenu", function (e) {
+            // return native menu if pressing control
+            if (e.ctrlKey) return;
+
+            //open menu
+            var $menu = $(settings.menuSelector)
+                .data("invokedOn", $(e.target))
+                .show()
+                .css({
+                    position: "absolute",
+                    left: getMenuPosition(e.clientX, 'width', 'scrollLeft'),
+                    top: getMenuPosition(e.clientY, 'height', 'scrollTop')
+                })
+                .off('click')
+                .on('click', 'a', function (e) {
+                    $menu.hide();
+
+                    var $invokedOn = $menu.data("invokedOn");
+                    var $selectedMenu = $(e.target);
+
+                    settings.menuSelected.call(this, $invokedOn, $selectedMenu);
+                });
+
+            return false;
+        });
+
+        //make sure menu closes on any click
+        $('body').click(function () {
+            $(settings.menuSelector).hide();
+        });
+    });
+
+    function getMenuPosition(mouse, direction, scrollDir) {
+        var win = $(window)[direction](),
+            scroll = $(window)[scrollDir](),
+            menu = $(settings.menuSelector)[direction](),
+            position = mouse + scroll;
+
+        // opening menu would pass the side of the page
+        if (mouse + menu > win && menu < mouse)
+            position -= menu;
+
+        return position;
+    }
+
+};
