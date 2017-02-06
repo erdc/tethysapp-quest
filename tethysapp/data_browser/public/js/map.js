99f1424434671e7969c3b7c58eefbf70462b7a73
Alan D. Snow <alan.d.snow@usace.army.mil>
2017-02-06 13:13:22 -0600

    fix select/deselect on map with drag box

--- tethysapp/data_browser/public/js/map.js
+++ tethysapp/data_browser/public/js/map.js
@@ -16,14 +16,11 @@ $.fn.serializeObject = function()
 };
 
 var map,
+    search_layer,
     search_select_interaction,
-    search_drag_box,
-    collection_select_interactions,
-    collection_drag_boxes,
+    collection_layers,
+    collection_select_interaction,
     SEARCH_LAYER_NAME = 'search-layer';
-
-collection_select_interactions = [];
-collection_drag_boxes = [];
 
 var get_map_extents = function(){
     var extent = map.getView().calculateExtent(map.getSize());
@@ -63,27 +60,15 @@ function update_collection_layer(collection){
 function deactivate_search_layer() {
     remove_layer(SEARCH_LAYER_NAME);
     map.removeInteraction(search_select_interaction);
-    map.removeInteraction(search_drag_box);
 //    search_select_interaction.getFeatures().clear();
 }
 
 function deactivate_collection_interaction() {
-
-    collection_select_interactions.forEach(function(collection_select_interaction) {
-        map.removeInteraction(collection_select_interaction);
-    });
-    collection_drag_boxes.forEach(function(collection_drag_box) {
-        map.removeInteraction(collection_drag_box);
-    });
+    map.removeInteraction(collection_select_interaction);
 }
 
 function activate_collection_interaction() {
-    collection_select_interactions.forEach(function(collection_select_interaction) {
-        map.addInteraction(collection_select_interaction);
-    });
-    collection_drag_boxes.forEach(function(collection_drag_box) {
-        map.addInteraction(collection_drag_box);
-    });
+    map.addInteraction(collection_select_interaction);
 }
 
 function toggle_feature_selection_by_id(feature_id, collection_name, show)
@@ -100,7 +85,6 @@ function toggle_feature_selection_by_id(feature_id, collection_name, show)
 }
 
 var load_map_layer = function(name, source_url, color, legend, callback) {
-    console.log(source_url);
     var layer_options = {'name': name};
 
     var geoJSONFormat = new ol.format.GeoJSON();
@@ -186,61 +170,18 @@ var load_map_layer = function(name, source_url, color, legend, callback) {
     // add vector layer to the map
     map.addLayer(layer);
 
-    var select_interaction = new ol.interaction.Select({
-        layers: [layer]
-    });
-
-    map.addInteraction(select_interaction);
-
-    // a DragBox interaction used to select features by drawing boxes
-    var drag_box = new ol.interaction.DragBox({
-      condition: ol.events.condition.shiftKeyOnly,
-      style: new ol.style.Style({
-        stroke: new ol.style.Stroke({
-          color: [0, 0, 255, 1]
-        })
-      })
-    });
-
-    map.addInteraction(drag_box);
-
-    drag_box.on('boxend', function() {
-      // features that intersect the box are added to the collection of
-      // selected features, and their names are displayed in the "info"
-      // div
-      var extent = dragBox.getGeometry().getExtent();
-
-      // Add the selected features to search layer
-      layer_source.forEachFeatureIntersectingExtent(extent, function(feature) {
-        select_interaction.getFeatures().push(feature);
-      });
-    });
-
     if(name == SEARCH_LAYER_NAME){
-        search_select_interaction = select_interaction;
-        search_drag_box = drag_box;
+        search_select_interaction = new ol.interaction.Select({
+            layers: [layer]
+        });
+        map.addInteraction(search_select_interaction);
+        search_layer = layer;
     }
     else
     {
-        // use the features Collection to detect when a feature is selected,
-        // the collection will emit the add event
-        var selected_features = select_interaction.getFeatures();
-        selected_features.on('add', function(event) {
-          var feature = event.target.item(0);
-          //$("tbody[data-collection_id='" + feature.get('collection') + "'] tr[data-feature_id='" + feature.get('name') + "']")
-          //.addClass('selected');
-        });
-
-        // when a feature is removed, clear the table selection
-        selected_features.on('remove', function(event) {
-          var feature = event.element;
-          $("tbody[data-collection_id='" + feature.get('collection') + "'] tr[data-feature_id='" + feature.get('name') + "']")
-          .removeClass('selected');
-        });
-
-        collection_select_interactions.push(select_interaction);
-        collection_drag_boxes.push(drag_box);
+        collection_layers[name] = layer;
     }
+
 
     if(legend){
         layer.setProperties({'tethys_legend_title': source_url});
@@ -291,9 +232,27 @@ $(function() { //wait for page to load
 
 map = TETHYS_MAP_VIEW.getMap();
 
+collection_layers = {};
+collection_select_interaction = new ol.interaction.Select();
+map.addInteraction(collection_select_interaction);
+
 // Load Collection Layers
 collections.forEach(add_collection_layer);
 
+// use the features Collection to detect when a feature is selected,
+// the collection will emit the add event
+collection_select_interaction.getFeatures().on('add', function(event) {
+  var feature = event.element;
+  $("tbody[data-collection_id='" + feature.get('collection')  + "'] tr[data-feature_id='" + feature.get('name') + "']")
+  .addClass('selected');
+});
+
+// when a feature is removed, clear the table selection
+collection_select_interaction.getFeatures().on('remove', function(event) {
+  var feature = event.element;
+  $("tbody[data-collection_id='" + feature.get('collection') + "'] tr[data-feature_id='" + feature.get('name') + "']")
+  .removeClass('selected');
+});
 
 // Bind events to controls
 var map_context_menu = new ContextMenu({
@@ -318,6 +277,58 @@ map_context_menu.on('beforeopen', function(evt){
 
 map.getViewport().addEventListener('contextmenu', function (evt) {
     evt.preventDefault();
+});
+
+// a DragBox interaction used to select features by drawing boxes
+var drag_box = new ol.interaction.DragBox({
+  condition: ol.events.condition.shiftKeyOnly,
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: [0, 0, 255, 1]
+    })
+  })
+});
+
+map.addInteraction(drag_box);
+
+drag_box.on('boxend', function() {
+  // features that intersect the box are added to the collection of
+  // selected features, and their names are displayed in the "info"
+  // div
+  var extent = drag_box.getGeometry().getExtent();
+
+  if ($('#manage-tab').parent().hasClass('active')) {
+
+      var feature_ids = [];
+      collection_select_interaction.getFeatures().forEach(function(selected_feature) {
+          feature_ids.push(selected_feature.getId());
+      });
+      $.each(collection_layers, function(layer_name, collection_layer) {
+          // Add the selected features to collection layer
+          collection_layer.getSource().forEachFeatureIntersectingExtent(extent, function(feature) {
+                if ($.inArray(feature.getId(), feature_ids) == -1)
+                {
+                    collection_select_interaction.getFeatures().push(feature);
+                }
+          });
+      });
+  }
+  else {
+      if(typeof search_layer != 'undefined')
+      {
+          var feature_ids = [];
+          search_select_interaction.getFeatures().forEach(function(selected_feature) {
+              feature_ids.push(selected_feature.getId());
+          });
+          // Add the selected features to search layer
+          search_layer.getSource().forEachFeatureIntersectingExtent(extent, function(feature) {
+              if ($.inArray(feature.getId(), feature_ids) == -1)
+              {
+                  search_select_interaction.getFeatures().push(feature);
+              }
+          });
+      }
+  }
 });
 
 }); //end of script
